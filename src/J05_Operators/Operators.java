2b4c33fd2900f13c5f23da34c17d5c29b565961a
rizwan-4308 <rizwan4308@gmail.com>
2025-07-19 11:59:34 +0530

    Added Operators

---
+++ src/J05_Operators/Operators.java
@@ -0,0 +1,45 @@
+package J05_Operators;
+
+public class Operators {
+    public static void main(String[] args){
+
+        // Arthmetic Operators
+
+        //int x =10;
+        int y = 3;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y;
+        //z = x / y;
+        //z = x % y;
+
+        // Augmented Assignment Operators
+
+        //x += y;
+        //x -= y;
+        //x *= y;
+        //x /= y;
+        //x %= y;
+
+        // Increment and Decrement Operators
+
+        int x = 1;
+
+        //x++;
+        //x++;
+
+        //x--;
+        //x--;
+
+        // System.out.println(x);
+
+        // Order of Operations [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+
+        System.out.println(result);
+
+    }
+}

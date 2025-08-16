7d5f86788202a957b3568b29812f270a412053bd
rizwan-4308 <rizwan4308@gmail.com>
2025-08-16 13:22:51 +0530

    Added Ternary Opertor

---
+++ src/J17_Ternary_Operator/ternaryOperator.java
@@ -0,0 +1,41 @@
+package J17_Ternary_Operator;
+
+public class ternaryOperator {
+    public static void main(String[] args){
+
+        // ternary operator ? = Return 1 of 2 values if a condition is true
+
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        /*
+        int score = 80;
+
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+
+        System.out.println(passOrFail);
+
+
+        int number = 3;
+
+        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+
+        System.out.println(evenOrOdd);
+
+
+        int hours = 13;
+
+        String timeOfDay = (hours < 12) ? "A.M." : "P.M.";
+
+        System.out.println(timeOfDay);
+         */
+
+        int income = 30000;
+
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+
+        System.out.println(taxRate);
+
+
+
+    }
+}
